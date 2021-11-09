010784e90bdb08d908fcc735d31d00b6af3fc5a7
gengen <gengen0630@gmail.com>
2021-11-09 16:57:05 +0900

    [add] train generator

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchsummary import summary
 
 
@@ -34,7 +37,7 @@ class ResidualSN(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, num_resblocks: int) -> None:
+    def __init__(self, num_resblocks: int = 7) -> None:
         super().__init__()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -59,7 +62,7 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, num_resblocks: int) -> None:
+    def __init__(self, num_resblocks: int = 6) -> None:
         super().__init__()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.noise_sigma = 0.01
@@ -88,29 +91,129 @@ class Discriminator(nn.Module):
 
 
 class Scyclone(nn.Module):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
+        # モデル
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.G_A2B = Generator(num_resblocks=7)
+        self.G_B2A = Generator(num_resblocks=7)
+        self.D_A = Discriminator(num_resblocks=6)
+        self.D_B = Discriminator(num_resblocks=6)
 
-    def forward(self, x):
-        return x
+        # 損失の係数
+        self.weight_cycle = 10
+        self.weight_identity = 1
 
-    def save(self, save_path):
+        # Hinge Lossのオフセット(SNGANは1.0, Scycloneは0.5)
+        # ref: https://arxiv.org/abs/2005.03334
+        self.offset = 0.5
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.G_A2B(x)
+
+    def save(self, save_path: str) -> None:
         torch.save(self.state_dict(), save_path)
 
-    def load(self, weights_path):
+    def load(self, weights_path: str) -> None:
         self.load_state_dict(torch.load(weights_path))
+
+    def train_g(self, batch: Tuple[torch.Tensor, torch.Tensor]) -> dict:
+        """
+        Generatorの訓練.
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): 真の変換元データと真の変換先データのペア
+
+        Returns:
+            dict: 損失の合計値と各損失を格納した辞書
+        """
+        real_A, real_B = batch
+
+        # Adversarial Loss (Hinge Loss)
+        fake_B = self.G_A2B(real_A)
+        pred_fake_B = self.D_B(torch.narrow(fake_B, 1, 16, 128))
+        loss_adv_G_A2B = torch.mean(F.relu(-1.0 * pred_fake_B))
+
+        fake_A = self.G_B2A(real_B)
+        pred_fake_A = self.D_A(torch.narrow(fake_A, 1, 16, 128))
+        loss_adv_G_B2A = torch.mean(F.relu(-1.0 * pred_fake_A))
+
+        # Cycle Consistency Loss (L1 Loss)
+        cycled_A = self.G_B2A(fake_B)
+        loss_cycle_ABA = F.l1_loss(cycled_A, real_A)
+
+        cycled_B = self.G_A2B(fake_A)
+        loss_cycle_BAB = F.l1_loss(cycled_B, real_B)
+
+        # Identity Loss (L1 Loss)
+        identity_B = self.G_A2B(real_B)
+        loss_identity_B = F.l1_loss(identity_B, real_B)
+
+        identity_A = self.G_B2A(real_A)
+        loss_identity_A = F.l1_loss(identity_A, real_A)
+
+        # Total Loss
+        loss_G = (
+            loss_adv_G_A2B + loss_adv_G_B2A
+            + self.weight_cycle * loss_cycle_ABA
+            + self.weight_cycle * loss_cycle_BAB
+            + self.weight_identity * loss_identity_A
+            + self.weight_identity * loss_identity_B
+        )
+
+        log = {
+            'Loss/G_total': loss_G,
+            'Loss/Adv/G_A2B': loss_adv_G_A2B,
+            'Loss/Adv/G_B2A': loss_adv_G_B2A,
+            'Loss/Cyc/A2B2A': loss_cycle_ABA,
+            'Loss/Cyc/B2A2B': loss_cycle_BAB,
+            'Loss/Id/A2A': loss_identity_A,
+            'Loss/Id/B2B': loss_identity_B,
+        }
+
+        out = {'Loss': loss_G, 'log': log}
+
+        return out
+
+    def train_d(self, batch: Tuple[torch.Tensor, torch.Tensor]) -> dict:
+        """
+        Discriminatorの訓練.
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): 真の変換元データと真の変換先データのペア
+
+        Returns:
+            dict: 損失の合計値と各損失を格納した辞書
+        """
+        real_A, real_B = batch
+
+        # Adversarial Loss (Hinge Loss)
+        ## D_A
+        ### Real Loss
+        pred_A_real = self.D_A(torch.narrow(real_A, 1, 16, 128))
+        loss_D_A_real = torch.mean(F.relu())
+
 
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # Generator
-    Generator = Generator(num_resblocks=7).to(device)
-    print('Generator:\n')
-    summary(Generator, input_size=(160, 80))  # 両橋16フレームずつカットしてDiscriminatorに渡す
+    dummy_input = torch.randn(1, 160, 80).to(device)
+    G = Generator(num_resblocks=7).to(device)
+    g_output = G(dummy_input)
+    print(f'G input:  {dummy_input.shape}')
+    print(f'G output: {g_output.shape}')
 
     # Discriminator
-    Discriminator = Discriminator(num_resblocks=6).to(device)
-    print('Discriminator:\n')
-    summary(Discriminator, input_size=(128, 80))
+    D = Discriminator(num_resblocks=6).to(device)
+    d_input = torch.narrow(g_output, 1, 16, 128)  # paddingの影響を回避するため，両橋16フレームずつカットしてDiscriminatorに渡す
+    d_output = D(d_input)
+    print(f'D input:  {d_input.shape}')
+    print(f'D output: {d_output.shape}')
+
+    # # モデルの概要
+    # print('Generator:\n')
+    # summary(G, input_size=(160, 80))
+    # print('Discriminator:\n')
+    # summary(D, input_size=(128, 80))
