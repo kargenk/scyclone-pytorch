b83037ab1a18e035f8756376bff3e9eb5e436a8a
gengen <gengen0630@gmail.com>
2021-12-15 16:05:29 +0900

    [add] clipping power

--- reconstruct_wave.py
+++ reconstruct_wave.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     resume_iter = 300000
 
     # log melspを抽出して160フレーム毎に分割
-    # log_melsp = wav_to_log_melsp('data/train_unique/jvs001/raw.wav')
-    log_melsp = wav_to_log_melsp('data/vcc2018/vcc2018_evaluation/VCC2SM1/30001.wav')
+    log_melsp = wav_to_log_melsp('data/test_common/jvs001/raw.wav')
+    # log_melsp = wav_to_log_melsp('data/vcc2018/vcc2018_evaluation/VCC2SM1/30001.wav')
     splited = time_split(log_melsp, 160)
 
     # [N, 80, 160]に整形
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 
     # 学習済みモデルの読み込み，log melspを変換
     model = Scyclone()
-    model.restore_models(resume_iter, '1129_vcc/models')
+    model.restore_models(resume_iter, '1126_jvs001_to_jvs015/models')
     output = model(input_tensor)
 
     # バッチ次元をつなげて正しい時間軸に整形
@@ -61,13 +61,13 @@ if __name__ == '__main__':
     clipped = clipping_power(time_concat)
 
     # 音声の保存
-    data = wav_from_melsp(time_concat)
-    # sf.write(f'result/jvs001_to_jvs015_at{resume_iter}_rec.wav', data, 24000, subtype='PCM_24')
-    sf.write(f'result/vcc2sm1_to_vcc2sf1_at{resume_iter}_cripped_rec.wav', data, 24000, subtype='PCM_24')
+    data = wav_from_melsp(clipped)
+    sf.write(f'result/jvs001_to_jvs015_at{resume_iter}_rec.wav', data, 24000, subtype='PCM_24')
+    # sf.write(f'result/vcc2sm1_to_vcc2sf1_at{resume_iter}_cripped_rec.wav', data, 24000, subtype='PCM_24')
 
     # スペクトログラムの保存
     plot_melsp(log_melsp, path='result/source_log_melsp')  # source
-    # log_melsp_trg = wav_to_log_melsp('data/train_unique/jvs015/raw.wav')
-    log_melsp_trg = wav_to_log_melsp('data/vcc2018/vcc2018_evaluation/VCC2SF1/30001.wav')
+    log_melsp_trg = wav_to_log_melsp('data/test_common/jvs015/raw.wav')
+    # log_melsp_trg = wav_to_log_melsp('data/vcc2018/vcc2018_evaluation/VCC2SF1/30001.wav')
     plot_melsp(log_melsp_trg, path='result/target_log_melsp')  # target
-    plot_melsp(time_concat, path=f'result/converted_log_melsp_{resume_iter}')  # converted
+    plot_melsp(clipped, path=f'result/converted_log_melsp_{resume_iter}')  # converted
